178898c8fe099d4a4ae3467aca121c3ab4553e9f
Manuel-arc <mnltavares17@gmail.com>
2022-05-17 14:45:38 +0100

    Update search_on_web.py

--- Extras/search_on_web.py
+++ Extras/search_on_web.py
@@ -4,9 +4,10 @@ import time
 from sys import platform
 import subprocess as sub
 
+linux_driver_path = '~/Documents/Github/Info-Gathering-Program/Extras/Drivers/Linux/'
+
 if platform == 'linux' or platform == 'linux2':
-    driver = webdriver.Firefox(
-        '~/Documents/Github/Info-Gathering-Program/Extras/Drivers/Linux/chromedriver')
+    driver = webdriver.Firefox(f'{linux_driver_path}gechodriver')
 elif platform == 'win32':
     driver = webdriver.Chrome("Extras\Drivers\Windows\chromedriver.exe")
 
