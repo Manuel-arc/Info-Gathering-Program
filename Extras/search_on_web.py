69876957666969427f71c24cc35630e374a84c5e
Manuel-arc <mnltavares17@gmail.com>
2022-05-17 12:58:57 +0100

    update

--- Extras/search_on_web.py
+++ Extras/search_on_web.py
@@ -1,12 +1,19 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+from sys import platform
 
-driver = webdriver.Chrome("Extras\chromedriver.exe")
+if platform == 'linux' or platform == 'linux2':
+    driver = webdriver.Chrome("Extras\Drivers\chromedriver")
+elif platform == 'win32':
+    driver = webdriver.Chrome("Extras\Drivers\chromedriver.exe")
+
+
 driver.get("https://www.google.com/")
 
+
 def launch():
-    
+
     time.sleep(100)
 
 
