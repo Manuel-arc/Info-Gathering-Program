6bf9d6acb62980a40d66ededd39153876d58bc68
Manuel-arc <mnltavares17@gmail.com>
2022-05-17 12:34:37 +0100

    update

--- Extras/search_on_web.py
+++ Extras/search_on_web.py
@@ -1,27 +1,13 @@
-from selenium import webdriver
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+driver = webdriver.Chrome("Extras\chromedriver.exe")
+driver.get("https://www.google.com/")
+
+def launch():
+    
+    time.sleep(100)
 
 
-def get_results(sreach_term):
-    url = "https://www.startpage.com"
-    browser = webdriver.PhantomJS()
-    browser.get(url)
-    search_box = browser.find_element_by_id("q")
-    search_box.send_keys(sreach_term)
-    search_box.submit()
-    try:
-        links = browser.find_elements_by_xpath(
-            "//ol[@class='web_regular_results']//h3//a")
-    except:
-        links = browser.find_elements_by_xpath("//h3//a")
-
-    results = []
-    for link in links:
-        href = link.get_attribute("href")
-        print(href)
-        results.append(href)
-
-    browser.close()
-    return results
-
-
-get_results("dog")
+launch()
