7abde3d7d4db0afed6dfe7f39bf31c541f422359
Manuel-arc <mnltavares17@gmail.com>
2022-05-17 18:35:11 +0100

    update

--- Extras/search_on_web.py
+++ Extras/search_on_web.py
@@ -1,25 +1,36 @@
+from sys import platform
+import time
+from unittest import result
 import selenium.webdriver as webdriver
 from selenium.webdriver.firefox.options import Options
-import time
-from sys import platform
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 linux_driver_path = '../Extras/Drivers/Linux/'
 
 if platform == 'linux' or platform == 'linux2':
-    options = Options()
-    options.headless = True
     driver = webdriver.Firefox()
 elif platform == 'win32':
-    driver = webdriver.Chrome("Extras\Drivers\Windows\chromedriver.exe")
+    #driver = webdriver.Chrome("Extras\Drivers\Windows\chromedriver.exe")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
-driver.get("https://www.google.com/")
+driver.get("https://www.startpage.com/")
 
+search_term = 'dog'
 
-def launch():
+search_box = driver.find_element_by_id('q')
+search_box.send_keys(search_term)
+search_box.submit()
 
-    time.sleep(100)
+links = driver.find_elements_by_class_name('result-link')
 
+results = []
+for link in links:
+    href = link.get_attribute('href')
+    print(href)
+    results.append(href)
 
-launch()
+time.sleep(20)
+
+driver.close()
