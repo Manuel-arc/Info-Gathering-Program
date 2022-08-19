8759aa4a10bae11d93158a963bbfa76630dfd3a8
Manuel-arc <mnltavares17@gmail.com>
2022-08-19 12:06:00 +0100

    a

--- Trabalho/tools/js_css_files.py
+++ Trabalho/tools/js_css_files.py
@@ -39,7 +39,11 @@ def host_to_scan():
 
     session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0'
 
-    html = session.get(url).content
+    try:
+        html = session.get(url).content
+    except:
+        print(bcolors.FAIL + "Invalid host!" + bcolors.ENDC)
+        host_to_scan()
 
     soup = bs(html, 'html.parser')
 
