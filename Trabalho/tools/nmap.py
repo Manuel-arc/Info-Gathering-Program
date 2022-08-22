8114042c94334c0633946eb3abe9d24eb88d6460
Manuel-arc <mnltavares17@gmail.com>
2022-08-22 18:00:15 +0100

    update nmap, add enum4linux

--- Trabalho/tools/nmap.py
+++ Trabalho/tools/nmap.py
@@ -30,16 +30,45 @@ nmap_commands = nmap()
 
 
 def main():
+    print()
+    print('1. Scan')
+    print('2. Help')
+    print('3. Go Back!')
+    choice = input(bcolors.UNDERLINE +
+                   "\nInfo Gathering/Nmap" + bcolors.ENDC + " > ")
 
-    print('1. Help')
-    print('2. Go Back!')
-    nmap_commands.host = input(bcolors.UNDERLINE +
-                               "\nInfo Gathering/Nmap" + bcolors.ENDC + " > Host: ")
+    if choice == '1' or choice.lower() == 'scan':
+        nmap_commands.host = input(bcolors.UNDERLINE +
+                                   "\nInfo Gathering/Nmap/Scan" + bcolors.ENDC + " > Host: ")
 
-    nmap_commands.flags = input(bcolors.UNDERLINE +
-                                "\nInfo Gathering/Nmap" + bcolors.ENDC + " > Flags: ")
+        nmap_commands.flags = input(bcolors.UNDERLINE +
+                                    "\nInfo Gathering/Nmap/Scan" + bcolors.ENDC + " > Flags: ")
+        nmap_scan(nmap_commands.host, nmap_commands.flags)
 
-    nmap_scan(nmap_commands.host, nmap_commands.flags)
+    elif choice == '2' or choice.lower() == 'help':
+        print()
+        print("--help")
+        print("man")
+        help = input(bcolors.UNDERLINE +
+                     "\nInfo Gathering/Nmap/Help" + bcolors.ENDC + " > ")
+
+        if help.lower() == '--help':
+            sub.run("nmap --help", shell=True)
+        elif help.lower() == 'man':
+            sub.run("man nmap", shell=True)
+        else:
+            print(bcolors.FAIL + "Invalid option!" + bcolors.ENDC)
+
+        print("\n\n-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-", end='\n')
+
+    elif choice == '3' or choice.lower() == 'exit':
+        main_page.menu()
+
+    else:
+        print()
+        print(bcolors.FAIL + "Invalid option!" + bcolors.ENDC)
+        print()
+        main()
 
 
 def nmap_scan(host, flags):
@@ -50,15 +79,12 @@ def nmap_scan(host, flags):
         data, result = nmap_commands.scan(host, flags, 'sudo')
 
     elif result == 0:
-        ''' a = re.findall(
-            r'\d{1,5}/\w{1,6}\s{1,9}\w{1,9}\s{1,9}\w{1,20}', nmap, re.MULTILINE) '''
-
         print('\n'+data)
 
         if "Note: Host seems down. If it is really up, but blocking our ping probes, try -Pn" in data:
             flags += ' -Pn'
-
-        data, _ = scan(host, flags)
+            data, _ = nmap_commands.scan(host, flags)
+            print(data)
 
         if '80/tcp' in data or '443/tcp' in data:
             print("Port 80 or 443 is open!")
