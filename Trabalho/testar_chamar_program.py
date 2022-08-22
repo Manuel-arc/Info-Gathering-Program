6f54924121dd3ac7b13e3940237a23a404630317
Manuel-arc <mnltavares17@gmail.com>
2022-08-22 14:38:50 +0100

    Update testar_chamar_program.py

--- Trabalho/testar_chamar_program.py
+++ Trabalho/testar_chamar_program.py
@@ -69,3 +69,19 @@ if __name__ == '__main__':
         print(nmap.returncode)
     else:
         quit()
+
+
+# Creating the HTML file
+file_html = open("demo.html", "w")
+# Adding the input data to the HTML file
+file_html.write('''<html>
+<head>
+<title>HTML File</title>
+</head> 
+<body>
+<h1>Welcome Finxters</h1>           
+<p>Example demonstrating How to generate HTML Files in Python</p> 
+</body>
+</html>''')
+# Saving the data into the HTML file
+file_html.close()
